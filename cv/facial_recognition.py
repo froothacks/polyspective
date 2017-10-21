6fb9a0bcb36db4b942d36fc3897e66c828895f0a
coolcom200 <leonfattakhov@yahoo.com>
2017-10-21 18:34:40 -0400

    added live video analysis

--- cv/facial_recognition.py
+++ cv/facial_recognition.py
@@ -10,17 +10,21 @@ while (cap.isOpened()):
     algorithms.init()
     while (cap.isOpened()):
         ret, frame = cap.read()
-        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        resized_gray = imutils.resize(gray, width=500)
-        cv2.imshow('frame', resized_gray)
+        # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # resized_gray = imutils.resize(gray, width=500)
+        # cv2.imshow('frame', resized_gray)
         resized_color = imutils.resize(frame, width=500)
         landmarks = algorithms.getFacePoints(resized_color)
-        mouth = utils.getMouthPoints(landmarks)
 
-        for coords in mouth["outer_lips"]:
-            cv2.circle(resized_color, (coords[0, 0], coords[0, 1]), 1, (255, 0, 0), thickness=-1)
-    cv2.imshow('color', resized_color)
-    if cv2.waitKey(1000) & 0xFF == ord('q'):
-        break
+        if type(landmarks) != type(None):
+            # print (landmarks)
+            print("faces found!")
+            mouth = utils.getMouthPoints(landmarks)
+
+            for coords in mouth["outer_lips"]:
+                cv2.circle(resized_color, (coords[0, 0], coords[0, 1]), 2, (255, 0, 0), thickness=-1)
+        cv2.imshow('color', resized_color)
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
 
 cap.release()
