1108e4567f77a73a7b0ce80e606e6956308cb44a
Calder White <calderwhite1@gmail.com>
2017-10-22 07:59:31 -0400

    Implimented mouth interpellation.

--- cv/Predictor.py
+++ cv/Predictor.py
@@ -7,7 +7,7 @@ import cv2
 
 
 class Constants:
-    DATA_PATH = "data"
+    DATA_PATH = "../data"
     TEST_PATH = DATA_PATH + "/test_data"
     LANDMARK_TRAINING_SET = "shape_predictor_68_face_landmarks.dat"
     FULLY_OPEN = 0.914772727275
@@ -62,10 +62,10 @@ class Algorithms:
         top_right = landmarks[64 - 1]
         bottom_right = landmarks[66 - 1]
         # calculating slopes
-        lt = min(abs((base_left[0, 1] - top_left[0, 1]) / (base_left[0, 0] - top_left[0, 0])),1)
-        lb = min(abs((base_left[0, 1] - bottom_left[0, 1]) / (base_left[0, 0] - bottom_left[0, 0])),1)
-        rt = min(abs((base_right[0, 1] - top_right[0, 1]) / (base_right[0, 0] - top_right[0, 0])),1)
-        rb = min(abs((base_right[0, 1] - bottom_right[0, 1]) / (base_right[0, 0] - bottom_right[0, 0])),1)
+        lt = min(abs((base_left[0, 1] - top_left[0, 1]) / max(base_left[0, 0] - top_left[0, 0],1)),1)
+        lb = min(abs((base_left[0, 1] - bottom_left[0, 1]) / max(base_left[0, 0] - bottom_left[0, 0],1)),1)
+        rt = min(abs((base_right[0, 1] - top_right[0, 1]) / max(base_right[0, 0] - top_right[0, 0],1)),1)
+        rb = min(abs((base_right[0, 1] - bottom_right[0, 1]) / max(base_right[0, 0] - bottom_right[0, 0],1)),1)
         # substact the face's slope from each slope
         face_slope = abs(Algorithms.getFaceSlope(landmarks))
         lt -= face_slope
@@ -120,8 +120,8 @@ class Face():
     def __init__(self,img):
         """A class to score a face."""
         self.landmarks = Algorithms.getFacePoints(img)
-        if len(self.landmarks) > 1:
-            print("Warning! More than 1 face found.")
+        ##if len(self.landmarks) > 1:
+        ##    print("Warning! More than 1 face found.")
     def get_mouth_score(self,landmark_index):
         """Calder's function to return a score from 0.0. to 1.0 on how open the face's mouth is."""
         return Algorithms.getMouthOpen(self.landmarks[landmark_index])
@@ -131,14 +131,47 @@ class Predictor(object):
     """The public class for use in the main program."""
     def __init__(self):
         Algorithms.init()
-        self.score_past = []
+        self.open_past = []
+        self.MAX_FRAMES = 10
+    def update_opens(self,scores):
+        if len(self.open_past) < len(scores):
+            for i in range(len(scores)):
+                self.open_past.append([])
+        for i in range(len(scores)):
+            self.open_past[i].append(scores[i])
+
+            if len(self.open_past[i]) > self.MAX_FRAMES:
+                self.open_past[i] = self.open_past[i][-self.MAX_FRAMES:]
     def next(self,frames):
         scores = [0.0]*len(frames)
+        # first scoring the current face
         for place,frame in enumerate(frames):
-            f = Face(frame)
-            if len(f.landmarks) > 0:
-                scores[place] = f.get_mouth_score(0)
-        return scores
+            fc = Face(frame)
+            opens = []
+            for f in range(len(fc.landmarks)):
+                opens.append(
+                    fc.get_mouth_score(f)
+                )
+            if len(opens)>0:scores[place] = max(opens)
+        self.update_opens(scores)
+        rscores = []
+        for i in range(len(frames)):
+            # now interpolate zero opens
+            for place,pt in enumerate(self.open_past[i]):
+                if pt == 0.0:
+                    if place-1 > 1 and place+1 < len(self.open_past[i]):
+                        pt = (self.open_past[i][place-1]+self.open_past[i][place+1])/2
+                        self.open_past[i][place]=pt
+            # finally, convert hour graph of open_past in a movement difference
+            diffs = []
+            #print(len(self.open_past[i]))
+            for j in range(len(self.open_past[i])-1):
+                diffs.append(
+                    abs(self.open_past[i][j-1]-self.open_past[i][j+1])
+                )
+            rscores.append(sum(diffs)/max(len(diffs),1))
+            #print(diffs)
+        return rscores
 def main():
     print("Loading...")
     im = cv2.imread(Constants.DATA_PATH + "/test_data/cw.jpg")
@@ -148,7 +181,7 @@ def main():
     ##cv2.waitKey(0)
     p = Predictor()
     print("Running...")
-    print(p.next([im,im2]))
+    p.next([im,im2])
 
 
 if __name__ == '__main__':
--- cv/facial_recognition.py
+++ cv/facial_recognition.py
@@ -1,12 +1,13 @@
 import cv2
-
+import imutils
 import predictor
+
+print("Running...")
 
 cap = cv2.VideoCapture('../data/James.mp4')
 # cap = cv2.VideoCapture(0)
 
-# will change this later to just Predictor.init() when Calder updates code
-predictor.Algorithms.init()
+p1 = predictor.Predictor()
 frame_interval = predictor.runDiagnostic()
 
 while (cap.isOpened()):
@@ -15,16 +16,18 @@ while (cap.isOpened()):
     counter = 0
     while (cap.isOpened()):
         ret, frame = cap.read()
+        rs = imutils.resize(frame,width=500)
         # gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
         # resized_gray = imutils.resize(gray, width=500)
         # cv2.imshow('frame', resized_gray)
         counter += 1
-        if counter == int(frame_interval/2):
-            mouths = predictor.getTestingImage(frame)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        if counter%15 == 0:
+            p1.next([frame])
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
             counter = 0
-            cv2.imshow('color', mouths)
+        cv2.imshow("color",rs)
+            #cv2.imshow('color', mouths)
 
 
 
